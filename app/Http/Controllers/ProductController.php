48ebfc2cc0263492e7d5e5504a56e2a1b65395f1
unknown <e.lillocastillo@gmail.com>
2021-03-01 00:15:42 -0300

    Modified ProductCollection JsonResource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,14 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Resources\Product\ProductResource;
-
+use App\Http\Resources\Product\ProductCollection;
 
 class ProductController extends Controller
 {
 
     public function index()
     {
-        return Product::all();
+        return ProductCollection::collection(Product::all());
     }
 
  
