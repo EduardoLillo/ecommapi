f5db421ac958c9d6e0c5fb2787b12ab4662ed70c
unknown <e.lillocastillo@gmail.com>
2021-03-08 15:53:15 -0300

    Completed Review

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -50,14 +50,25 @@ class ReviewController extends Controller
     }
 
 
-    public function update(Request $request, Review $review)
+    public function update(Request $request, Product $product , Review $review)
     {
-        //
+        $review->update($request->all());
+
+        //$product->reviews()->save($review);
+
+        return response([
+        
+                    'data' => new ReviewResource($review)
+        
+                ],Response::HTTP_CREATED);
+    
     }
 
    
-    public function destroy(Review $review)
+    public function destroy(Product $product, Review $review)
     {
-        //
+       
+        $review->delete();
+        return response(null, Response::HTTP_NO_CONTENT);    
     }
 }
